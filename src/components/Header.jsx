0634c21f3500ec48751f2c7d64e5f5714bdf4cec
saivivek23 <raavisaivivek@gmail.com>
2023-11-13 17:54:11 +0530

    changed progress bar radius

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
         <img src={image} style={{ backgroundColor: `${color}` }} />
         <p>{title}</p>
       </div>
-      <div className={styles.modes}>Modes</div>
+      {/* <div className={styles.modes}>Modes</div> */}
     </div>
   );
 }
